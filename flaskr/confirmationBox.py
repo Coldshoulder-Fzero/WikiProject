29b63ba4b4d31ddb94c59d22e727c5715de527ac
Joseph Gabrie <josephgabrie@techexchange.in>
2023-04-10 19:40:56 +0000

    confirmation box

---
+++ flaskr/confirmationBox.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Add your logic to save the changes to the wiki
+        return redirect(url_for('pages'))
+
+    return render_template('pages.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
